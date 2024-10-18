62d9023cec609ed56f4aad4923c034fa05f8fdf7
HooFoo <lHooFool@gmail.com>
2024-10-18 17:40:00 +0400

    Buttons callback changes

--- webhook.php
+++ webhook.php
@@ -21,36 +21,11 @@ if ($update === null) {
 // Extract basic data
 $chat_id = $update['message']['chat']['id'] ?? null;
 $text = $update['message']['text'] ?? null;
+$callback_query_id = $update['callback_query']['id'] ?? null;
 $callback_data = $update['callback_query']['data'] ?? null;
 
 // Log the incoming update
 error_log("Incoming update: " . print_r($update, true), 3, $log_file);
-
-// Check if the update is a callback query
-if (isset($update['callback_query'])) {
-    $callback_data = $update['callback_query']['data'];
-    $chat_id = $update['callback_query']['message']['chat']['id'];
-
-    // Handle different callback data
-    if ($callback_data === "check_balance") {
-        include 'modules/show_balance.php';
-        show_balance($chat_id);
-    } elseif (strpos($callback_data, 'refill_') !== false) {
-        include 'modules/refill_balance.php';
-        handle_refill_callback($chat_id, $callback_data);
-    } elseif ($callback_data === "request_feature") {
-        include 'modules/feature_request.php';
-        handle_feature_request($chat_id);
-    }
-} elseif ($text === "/start") {
-    include 'modules/start_session.php';
-    start_session($chat_id);
-} elseif (isset($update['message']['successful_payment'])) {
-    include 'modules/refill_balance.php';
-    handle_successful_payment($chat_id, $update['message']['successful_payment']);
-} else {
-    send_message($chat_id, "Пожалуйста, используйте одну из доступных опций.");
-}
 
 // Function to send messages to Telegram
 function send_message($chat_id, $text, $reply_markup = null) {
@@ -81,5 +56,58 @@ function send_message($chat_id, $text, $reply_markup = null) {
     if ($response === false) {
         error_log("Failed to send message: $http_response_header", 3, $log_file);
     }
+}
+
+// Function to answer callback queries
+function answer_callback_query($callback_query_id, $text = '') {
+    global $telegram_token;
+
+    $url = "https://api.telegram.org/bot$telegram_token/answerCallbackQuery";
+    $data = [
+        'callback_query_id' => $callback_query_id,
+        'text' => $text,
+        'show_alert' => false // Set to true if you want to show an alert message
+    ];
+
+    $options = [
+        'http' => [
+            'header'  => "Content-Type: application/json\r\n",
+            'method'  => 'POST',
+            'content' => json_encode($data)
+        ]
+    ];
+
+    // Send the request
+    file_get_contents($url, false, stream_context_create($options));
+}
+
+// Check if the update is a callback query
+if (isset($update['callback_query'])) {
+    $chat_id = $update['callback_query']['message']['chat']['id'];
+    $callback_query_id = $update['callback_query']['id'];
+    $callback_data = $update['callback_query']['data'];
+
+    // Handle different callback data
+    if ($callback_data === "check_balance") {
+        include 'modules/show_balance.php';
+        show_balance($chat_id);
+        answer_callback_query($callback_query_id, "Баланс проверен.");
+    } elseif (strpos($callback_data, 'refill_') !== false) {
+        include 'modules/refill_balance.php';
+        handle_refill_callback($chat_id, $callback_data);
+        answer_callback_query($callback_query_id, "Запрос на пополнение баланса обработан.");
+    } elseif ($callback_data === "request_feature") {
+        include 'modules/feature_request.php';
+        handle_feature_request($chat_id);
+        answer_callback_query($callback_query_id, "Запрос на функцию обработан.");
+    }
+} elseif ($text === "/start") {
+    include 'modules/start_session.php';
+    start_session($chat_id);
+} elseif (isset($update['message']['successful_payment'])) {
+    include 'modules/refill_balance.php';
+    handle_successful_payment($chat_id, $update['message']['successful_payment']);
+} else {
+    send_message($chat_id, "Пожалуйста, используйте одну из доступных опций.");
 }
 ?>
